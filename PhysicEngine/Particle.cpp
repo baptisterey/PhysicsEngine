3fd70b553f8c3293587663cdda83ca2f1a986a5d
Civetdelapin <bababapt@gmail.com>
2019-09-10 15:54:13 -0400

    added class Particle from Loys branch

---
+++ PhysicEngine/Particle.cpp
@@ -0,0 +1,51 @@
+#include "Particle.h"
+#include <math.h> 
+
+Particle::Particle() {
+	position = Vector3();
+	velocity = Vector3();
+	acceleration = Vector3();
+
+	mass = 0;
+	invertedMass = 0;
+}
+
+
+Particle::Particle(Vector3 pos, Vector3 vel, Vector3 acc, float mass) : position(pos), velocity(vel), acceleration(acc) {
+	setMass(mass);
+}
+
+
+Particle::~Particle() {
+}
+
+void Particle::setMass(float _mass)
+{
+	mass = _mass;
+	invertedMass = 1 / _mass;
+}
+
+float Particle::getMass() { return mass; }
+
+float Particle::getInvertedMass() { return invertedMass; }
+
+void Particle::updateAcceleration(Vector3 gravity) {
+	acceleration = gravity;
+}
+
+void Particle::updateVelocity(float fTime, float damp) {
+	velocity = velocity * pow(damp, fTime) + acceleration * fTime;
+
+}
+
+void Particle::updatePosition(float fTime, float damp) {
+	position = position + velocity * fTime;
+
+}
+
+
+void Particle::updateParticle(float fTime, float damp, Vector3 gravity) {
+	updatePosition(fTime, damp);
+	updateVelocity(fTime, damp);
+	updateAcceleration(gravity);
+}
---
+++ PhysicEngine/Particle.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Vector3.h"
+class Particle
+{
+protected:
+	float mass, invertedMass;
+
+public:
+
+	Vector3 position, velocity, acceleration;
+
+
+	Particle();
+	Particle(Vector3 pos, Vector3 vel, Vector3 acc, float mass);
+	~Particle();
+
+	void setMass(float mass);
+	float getMass();
+	float getInvertedMass();
+
+
+	void updateAcceleration(Vector3 gravity);
+	void updateVelocity(float fTime, float damp);
+	void updatePosition(float fTime, float damp);
+	void updateParticle(float fTime, float damp, Vector3 gravity);
+
+
+};
