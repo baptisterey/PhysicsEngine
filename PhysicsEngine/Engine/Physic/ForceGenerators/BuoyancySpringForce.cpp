882b20a6638502011c50a480c808a5da155f0139
Bertrand Gilbert-Collet <bertrand.gc@gmail.com>
2019-10-02 15:22:07 -0400

    Added updateForce

--- PhysicsEngine/Engine/Physic/ForceGenerators/BuoyancySpringForce.cpp
+++ PhysicsEngine/Engine/Physic/ForceGenerators/BuoyancySpringForce.cpp
@@ -4,7 +4,8 @@ BuoyancySpringForce::BuoyancySpringForce()
 {
 }
 
-BuoyancySpringForce::BuoyancySpringForce(float maxDepth, float objectVolume, float liquidLevel, float liquidDensity) : maxDepth(maxDepth), objectVolume(objectVolume), liquidLevel(liquidLevel), liquidDensity(liquidDensity)
+BuoyancySpringForce::BuoyancySpringForce(float maxDepth, float objectVolume, float liquidLevel, float liquidDensity)
+	: maxDepth(maxDepth), objectVolume(objectVolume), liquidLevel(liquidLevel), liquidDensity(liquidDensity)
 {
 }
 
@@ -14,6 +15,20 @@ BuoyancySpringForce::~BuoyancySpringForce()
 {
 }
 
-void BuoyancySpringForce::updateForce()
+void BuoyancySpringForce::UpdateForce(IPhysicComponent* physicComponent, float deltaTime)
 {
+	//Compute d, used to determine how the object interacts with the liquid
+	d = (physicComponent. - liquidLevel - maxDepth) / 2 * maxDepth;
+
+	if (d <= 0) {
+		physicComponent->AddForce()
+	}
+	else if (d >= 1) {
+
+	}
+	else {
+
+	}
+
+
 }
--- PhysicsEngine/Engine/Physic/ForceGenerators/BuoyancySpringForce.h
+++ PhysicsEngine/Engine/Physic/ForceGenerators/BuoyancySpringForce.h
@@ -2,6 +2,7 @@
 #include "../Base/IForceGenerator.h"
 #include "../Base/IPhysicComponent.h"
 #include "../../Math/Vector3.h"
+#include "../../Base/Entity.h"
 
 class BuoyancySpringForce : public IForceGenerator
 {
@@ -11,11 +12,15 @@ public:
 	float liquidLevel;
 	float liquidDensity;
 
+	Entity* attachedEntity; //Remote entity
+	Vector3 anchor;
+
 	BuoyancySpringForce();
 	BuoyancySpringForce(float maxDepth, float objectVolume, float liquidLevel, float liquidDensity);
 
+
 	~BuoyancySpringForce();
 
-	void updateForce();
+	void UpdateForce(IPhysicComponent* physicComponent, float deltaTime);
 
 }
