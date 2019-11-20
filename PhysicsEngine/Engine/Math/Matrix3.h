ad6828c9d25b1f390d43f8a8ba29628cca44089a
Yoann HAFFNER <yoyofrom68@gmail.com>
2019-11-20 01:45:34 -0500

    Comments matrix3 and matrix4

--- PhysicsEngine/Engine/Math/Matrix3.h
+++ PhysicsEngine/Engine/Math/Matrix3.h
@@ -5,22 +5,66 @@
 class Matrix3
 {
 public:
-
-	//Constructor
+	/// <summary>
+	/// Constructor (identity)
+	/// </summary>
 	Matrix3();
+
+	/// <summary>
+	/// Constructor from a complete array a float
+	/// </summary>
+	/// <param name="matrixTable"></param>
     Matrix3(std::vector<float> matrixTable);
+
+	/// <summary>
+	/// Constructor from indexed values
+	/// </summary>
+	/// <param name="i0"></param>
+	/// <param name="i1"></param>
+	/// <param name="i2"></param>
+	/// <param name="i3"></param>
+	/// <param name="i4"></param>
+	/// <param name="i5"></param>
+	/// <param name="i6"></param>
+	/// <param name="i7"></param>
+	/// <param name="i8"></param>
     Matrix3(float i0, float i1, float i2, float i3, float i4, float i5, float i6, float i7, float i8);
+
+	/// <summary>
+	/// Constructor from existing matrix
+	/// </summary>
+	/// <param name="copyMatrix"></param>
     Matrix3(Matrix3& copyMatrix);
 
-	//Methode
+
+
+	/// <summary>
+	/// Returns matrix determinant
+	/// </summary>
+	/// <returns></returns>
     float matrix3Det();
+
+	/// <summary>
+	/// Returns inverted matrix
+	/// </summary>
+	/// <returns></returns>
     Matrix3 matrix3Inverse();
+
+	/// <summary>
+	/// Returns transposed matrix
+	/// </summary>
+	/// <returns></returns>
     Matrix3 matrix3Transpose();
 	
-	//Operator
+
+	// ---- OPERATOR OVERLOADING ----
 	Vector3 operator*(Vector3 const& v);
 	Matrix3 operator*(Matrix3 const& matrix);
 	Matrix3 operator*(float value);
+	// ------------------------------
 
+	/// <summary>
+	/// Matrix values
+	/// </summary>
 	std::vector<float> matrix;
 };
--- PhysicsEngine/Engine/Math/Matrix4.h
+++ PhysicsEngine/Engine/Math/Matrix4.h
@@ -6,24 +6,84 @@
 class Matrix4
 {
 public:
-
-	//Constructor
+	/// <summary>
+	/// Constructor (identity)
+	/// </summary>
     Matrix4();
+
+	/// <summary>
+	/// Constructor from another matrix
+	/// </summary>
+	/// <param name="copyMatrix"></param>
     Matrix4(Matrix4& copyMatrix);
+
+	/// <summary>
+	/// Constructor from an array a values
+	/// </summary>
+	/// <param name="matrixTable"></param>
     Matrix4(std::vector<float> matrixTable);
 
-	//Methode
+
+
+	/// <summary>
+	/// Returns matrix determinant
+	/// </summary>
+	/// <returns></returns>
     float matrix4Det();
+
+	/// <summary>
+	/// Returns inverted matrix
+	/// </summary>
+	/// <returns></returns>
     Matrix4 matrix4Inverse();
+
+	/// <summary>
+	/// Returns transposed matrix
+	/// </summary>
+	/// <returns></returns>
     Matrix4 matrix4Transpose();
+
+	/// <summary>
+	/// Returns a projection matrix for the given parameters
+	/// </summary>
+	/// <param name="FOV">Angles of the FOV</param>
+	/// <param name="Aspect">Aspect raio (width/height)</param>
+	/// <param name="ZNear">Distance to the near plane of the camera</param>
+	/// <param name="ZFar">Distance to the far plane of the camera</param>
+	/// <returns></returns>
 	static Matrix4 PerspectiveFov(float FOV, float Aspect, float ZNear, float ZFar);
+
+	/// <summary>
+	/// Returns a matrix rotation to rotate towards looking to a point
+	/// </summary>
+	/// <param name="Eye">Current direction the eye is looking at (or current rotation)</param>
+	/// <param name="At">Point we want to look at</param>
+	/// <param name="Up">Current local UP vector</param>
+	/// <returns></returns>
 	static Matrix4 LookAt(const Vector3& Eye, const Vector3& At, const Vector3& Up);
+
+	/// <summary>
+	/// Apply the current matrix rotation to a point
+	/// </summary>
+	/// <param name="point"></param>
+	/// <returns></returns>
 	Vector3 TransformPoint(const Vector3& point) const;
+
+	/// <summary>
+	/// Conversion to give the matrix to a GLSL shader
+	/// </summary>
+	/// <param name="temp"></param>
 	void getTransformedMatrix(float temp[4][4]);
 
-	//Operator
+
+	// ---- OPERATOR OVERLOADING ----
 	Matrix4 operator*(Matrix4 const& matrix);
     Vector3 operator*(Vector3 const& v);
+	// ------------------------------
 
+
+	/// <summary>
+	/// Matrix values
+	/// </summary>
 	std::vector<float> matrix;
 };
